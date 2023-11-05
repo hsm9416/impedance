e3ed4c79edeff87d614b225f0ea3e23e32c021f6
hsm9416 <gktjdals9416@hanyang.ac.kr>
2023-11-05 16:19:11 +0900

    저장

--- test.py
+++ test.py
@@ -11,103 +11,74 @@ ser = serial.Serial(
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE
 )
-class ImpedanceController:
-    def __init__(self, mass, damping, stiffness):
-        """
-        Initialize the 1-DoF Impedance Controller with the given parameters.
 
-        :param mass: Mass of the system (m)
-        :param damping: Damping coefficient (b)
-        :param stiffness: Stiffness coefficient (k)
-        """
+class ImpedanceControl:
+    
+    def __init__(self,mass,stiffness,damping):
         self.mass = mass
-        self.damping = damping
         self.stiffness = stiffness
-        self.position = 0
-        self.velocity = 0
-        self.force = 0
+        self.damping = damping
+    
+    def impedance_control(self,desired_angle,desired_velocity,angle,velocity):
+        tau = self.stiffness*(desired_angle-angle) + self.damping*(desired_velocity-velocity)
+        return tau
 
-    def update(self, desired_position, actual_position, actual_velocity, dt):
-        """
-        Update the controller state based on the desired position, actual position,
-        actual velocity, and time step.
+    def update_state(self,angle,velocity):
+        self.angle = angle
+        self.velocity = velocity
 
-        :param desired_position: Desired position (setpoint)
-        :param actual_position: Current position of the system
-        :param actual_velocity: Current velocity of the system
-        :param dt: Time step for the update
-        :return: Commanded force to apply to the system
-        """
-        position_error = desired_position - actual_position
-        self.force = (self.stiffness * position_error - 
-                      self.damping * actual_velocity)
-        self.update_state(dt)
-        return self.force
-
-    def update_state(self, dt):
-        """
-        Update the internal state of the system (position and velocity) based on
-        the applied force and time step.
-
-        :param dt: Time step for the update
-        """
-        acceleration = self.force / self.mass
-        self.velocity += acceleration * dt
-        self.position += self.velocity * dt
-
+def angle_limit(angle,tau):
+    if angle > 60:
+        tau = 0
+    elif angle < -60:
+        tau = 0
+    return tau
 
 
 def send_left_command(l_tau):
     l_command = f"\r\nCURRENT?L#{l_tau}|".encode()
-    ser.write(l_command)
     return l_command
 
 def send_right_command(r_tau):
     r_command = f"\r\nCURRENT?R#{r_tau}|".encode()
-    ser.write(r_command)
     return r_command
 
-l_controller = ImpedanceController(1, 1, 1)
-l_desired_position = 0
-l_actual_position = uart.l_hip_angle
-l_actual_velocity = uart.l_hip_velocity
-dt = 0.01
-l_tau = l_controller.update(l_desired_position, l_actual_position, l_actual_velocity, dt)
-l_controller.update_state(dt)
-
-r_controller = ImpedanceController(1, 1, 1)
-r_desired_position = 0
-r_actual_position = uart.r_hip_angle
-r_actual_velocity = uart.r_hip_velocity
-dt = 0.01
-r_tau = r_controller.update(r_desired_position, r_actual_position, r_actual_velocity, dt)
-r_controller.update_state(dt)
-try:
-    # 사용자로부터 입력받기
-    user_input = input("Enter '1' to send the command: ")
-
-    while True:   # 사용자가 '1'을 입력하면 명령 전송
-        if user_input == '1':
+def main():
+    try:
+        # 사용자로부터 입력받기
+        user_input = input("Enter '1' to send the command: ")
+    
+        while True:   # 사용자가 '1'을 입력하면 명령 전송
+            if user_input == '1':
             
-            r_tau = -150
-            l_command = send_left_command(l_tau)
-            r_command = send_right_command(r_tau)
-            ser.write(l_command)
-            ser.write(r_command)
-            print('===============================')
-            # time.sleep(0.5) 
+                l_tau = 200
+                r_tau = -200
+                l_command = send_left_command(l_tau)
+                r_command = send_right_command(r_tau)
+                ser.write(l_command) 
+                ser.write(r_command)
+                print(f"l_tau : {l_tau}")
+                print(f"r_tau : {r_tau}")
+                print('===============================')
+    
+    except KeyboardInterrupt:
+        l_tau = 0
+        r_tau = 0
+        l_command = send_left_command(l_tau)
+        r_command = send_right_command(r_tau)
+        command = "\r\nTOGGLE#0\r\n".encode()  # 문자열을 바이트로 인코딩
+        ser.write(command)
+        ser.write(l_command)
+        ser.write(r_command)
+        print("stopped")
+        time.sleep(0.5) 
+        print("\nProgram terminated by user.")
+    
+    finally:
+        ser.close()  # 시리얼 포트 닫기
+        print("Serial port closed.")
 
-except KeyboardInterrupt:
-    l_tau = 0
-    r_tau = 0
-    l_command = send_left_command(l_tau)
-    r_command = send_right_command(r_tau)
-    ser.write(l_command)
-    ser.write(l_command)
-    print("stopped")
-    time.sleep(0.5) 
-    print("\nProgram terminated by user.")
-finally:
-    ser.close()  # 시리얼 포트 닫기
-    print("Serial port closed.")
+
+if __name__ == "__main__":
+    main()
  
--- uart.py
+++ uart.py
@@ -1,42 +1,92 @@
-
 import serial
 import time
+import test
 
 # 시리얼 객체 생성
-ser = serial.Serial(
-    port="/dev/ttyTHS0",
-    baudrate=115200,
-    bytesize=serial.EIGHTBITS,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE
-)
+def main():
 
-try:
-        command = "\r\nTOGGLE#1\r\n".encode()  # 문자열을 바이트로 인코딩
-        ser.write(command)
-        print("Command sent, waiting for response...")
-        # 데이터 읽기
-        while True:
-            data = ser.readline().decode('utf-8', errors="ignore").strip()  # 데이터 읽고, 디코딩
-            if data:  # 데이터가 비어있지 않으면 출력
+    try:
+        with serial.Serial(
+            port="/dev/ttyTHS0",
+            baudrate=115200,
+            bytesize=serial.EIGHTBITS,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE
+        ) as ser:
+            command_1 = "\r\nTOGGLE#1\r\n".encode()  # 문자열을 바이트로 인코딩
+            ser.write(command_1)
+            
+            print("Command sent")
+
+            # 데이터 읽기
+            while True:
+                start_time = time.time()
+                data = ser.readline().decode('utf-8', errors="ignore").strip()  # 데이터 읽고, 디코딩
+                # 데이터가 비어있지 않으면 출력
                 split_data = data.split('|')
-                Robot_time,l_hip_angle,r_hip_angle,l_hip_velocity,r_hip_velocity,l_hip_torque,r_hip_torque,l_hip_targetspeed,r_hip_targetspeed,control_mode,control_interval=split_data
+                Robot_time = split_data[0]
+                l_hip_angle = float(split_data[1])
+                r_hip_angle = float(split_data[2])
+                l_hip_velocity = float(split_data[3])
+                r_hip_velocity = float(split_data[4])
+                l_hip_current = float(split_data[5])
+                r_hip_current = float(split_data[6])
+                contorl_mode = split_data[9]
+                control_interval = split_data[10]
 
-                # print(f"Robot_time: {Robot_time}")
+                
+                desired_angle = 0
+                desired_velocity = 0
+                mass = 1
+                stiffness = 1
+                damping = 1
+
+                l_controller = test.ImpedanceControl(mass,stiffness,damping)
+                r_controller = test.ImpedanceControl(mass,stiffness,damping)
+
+             # 명령 생성 및 전송
+                try:
+                    # l_tau = l_controller.impedance_control(desired_angle,desired_velocity,l_hip_angle,l_hip_velocity)
+                    l_tau = 200
+                    r_tau = -200
+                    limit_l_tau = test.angle_limit(l_hip_angle,l_tau)
+                    l_command = test.send_left_command(limit_l_tau)
+                    ser.write(l_command)
+
+                    # r_tau = r_controller.impedance_control(desired_angle,desired_velocity,r_hip_angle,r_hip_velocity)
+                    limit_r_tau = test.angle_limit(r_hip_angle,r_tau)
+                    r_command = test.send_right_command(limit_r_tau)
+                    ser.write(r_command)
+
+                    l_controller.update_state(l_hip_angle,l_hip_velocity)
+                    r_controller.update_state(r_hip_angle,r_hip_velocity)
+                    # time.sleep(0.05)
+  
+                    
+
+                except serial.SerialException as e:
+                    print(f"Serial communication error: {e}")
+                    break
+                
+
                 print(f"l_hip_angle: {l_hip_angle}")
                 print(f"r_hip_angle: {r_hip_angle}")
-                print(f"l_hip_velocity: {l_hip_velocity}")
-                print(f"l_hip_velocity: {r_hip_velocity}")
-                print(f"l_hip_torque: {l_hip_torque}")
-                print(f"r_hip_torque: {r_hip_torque}")
-                # print(f"l_hip_targetspeed: {l_hip_targetspeed}")
-                # print(f"r_hip_targetspeed: {r_hip_targetspeed}")
-                # print(f"control_mode: {control_mode}")
-                # print(f"control_interval: {control_interval}")
-                print("=====================================")
+                print(f"l_tau: {l_tau}")
+                print(f"r_tau: {r_tau}")
+                print(f"l_hip_current: {l_hip_current}")
+                print(f"r_hip_current: {r_hip_current}")
+                print(f"contorl_mode: {contorl_mode}")
+                print(f"control_interval: {control_interval}")
+                print(f"elapsed_time: {}")
 
-except KeyboardInterrupt:
-    print("\nProgram terminated by user.")
-finally:
-    ser.close()  # 시리얼 포트 닫기
-    print("Serial port closed.")
+
+
+
+    except KeyboardInterrupt:
+        print("Manual interrupt received, stopping...")
+    finally:
+        print("Program ended.")
+
+if __name__ == "__main__":
+    main()
+
